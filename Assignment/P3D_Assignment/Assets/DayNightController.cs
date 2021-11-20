4d019227e013761a72051c661d13a4fd724e0cc5
Daniel Newsom <danielgnewsom@gmail.com>
2021-11-20 13:28:07 +0000

    Danny - Chandelier - remodelled

--- Assignment/P3D_Assignment/Assets/DayNightController.cs
+++ Assignment/P3D_Assignment/Assets/DayNightController.cs
@@ -10,40 +10,65 @@ public class DayNightController : MonoBehaviour
     [SerializeField] private bool isDaytime = false;
     [SerializeField] private VolumeProfile dayProfile;
     [SerializeField] private VolumeProfile nightProfile;
-
+    
     private Light[] lights;
     private Volume volume;
     private Animator animator;
-
+    private float animationSpeed;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         volume = GetComponentInChildren<Volume>();
         lights = FindObjectsOfType<Light>();
+        animationSpeed = animator.GetFloat("Speed");
         SetNighttime();
     }
 
-    public void UpdateAnimator()
+    private void UpdateAnimator()
     {
         animator.SetBool("IsDaytime",isDaytime);
     }
 
-    internal void SetDaytime()
+    private void SetDaytime()
     {
         isDaytime = true;
         volume.profile = dayProfile;
+        UpdateAnimator();
     }
 
-    internal void SetNighttime()
+    private void SetNighttime()
     {
         isDaytime = false;
         volume.profile = nightProfile;
+        UpdateAnimator();
     }
-
+    [ContextMenu("Toggle Day/Night")]
     public void ToggleDayNight()
     {
         isDaytime = !isDaytime;
+        if (isDaytime)
+        {
+            SetDaytime();
+        }
+        else
+        {
+            SetNighttime();
+        }
+    }
+
+    [ContextMenu("Skip To Day")]
+    public void SkipToDaytime()
+    {
+        SetDaytime();
+        animator.SetFloat("Speed",5f);
+    }
+    
+    [ContextMenu("Skip To Night")]
+    public void SkipToNighttime()
+    {
+        SetNighttime();
+        animator.SetFloat("Speed",5f);
     }
 
     public void EnableHouseLights()
@@ -66,5 +91,11 @@ public class DayNightController : MonoBehaviour
                 light.enabled = false;
             }
         }
+        
+    }
+
+    public void ResetSpeed()
+    {
+        animator.SetFloat("Speed",animationSpeed);
     }
 }
